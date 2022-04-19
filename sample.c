4d4dee3fbbabebafd06e8ba2b3f8fbd7cd9c0ed2
Daniel Ramos III <daniel_ramos@dlsu.edu.ph>
2022-04-19 15:57:44 +0800

    remove everything

--- sample.c
+++ sample.c
@@ -2,29 +2,5 @@
 
 int main()
 {
-    initscr();
-    noecho();
-    nodelay(stdscr, TRUE);
-    keypad(stdscr, TRUE);
-    printw("Hello world%c%c\n", '!', '!');
-    int ch = getch();
-
-    int y = 1;
-
-    while(ch != 'q'){
-        if(ch == KEY_DOWN) {
-            y++;
-        } else if(ch == KEY_UP) {
-            y--;
-        }
-        
-        move(y, 0);
-        printw("%d\n", ch);
-        ch = getch();
-        refresh();
-    }
-
-    endwin();
-
     return 0;
 }
