f39bf18f5de1b991cc89dbf7e988c2e17a3ec85b
gwnrocs <gwen.roco@dlsu.edu.ph>
2022-05-31 15:54:43 +0800

    📌 added reader functions

--- src/pgdocs.h
+++ src/pgdocs.h
@@ -26,6 +26,37 @@ typedef struct {
     char strInput[MAX_CHAR];
 } ActiveScreen;
 
+typedef struct {
+    const char *name;
+    char *data;
+} MetadataItem;
+
+typedef enum {
+    CLEAN, 
+    ANALYZE
+} Mode;
+
+typedef struct {
+    Mode mode;                  // processing mode (i.e. clean or analyze)
+    FILE *inFile;               // file pointer to input 
+    FILE *outFile;              // file pointer to output
+    unsigned int options;       // chosen option for cleaning or analysis
+    Metadata metadata;          // info about the document
+    char *inData;               // string of data to be processed
+    TokenList *tokenList;       // tokenized version of the input
+    char *outData;              // string of data to be reported
+    AdditionalOptions addOpts;  // additional options
+} Summary;
+
+typedef struct {
+    bool isInitialized;         // whether config is initialized or not
+    char *rawDocumentPath;      // path of raw PG documents
+    char *cleanedDocumentPath;  // path of cleaned documents
+    char *analysisOutputPath;   // path of output files (i.e., word count,...)
+    int numChar;                // number of character to be read from content
+    bool multiSelect;           // allow to choose more than one option
+} Config;
+
 void initialize_screens(Screen screens[]);
 void activate_screen(ActiveScreen* active, Screen screens[]);
 void __validate_screen_option(ActiveScreen *active);
@@ -33,5 +64,11 @@ void go_to_screen(Screen *screens[], ActiveScreen *active);
 void get_int(ActiveScreen *active);
 void get_str(ActiveScreen *active);
 void print_metadata(char *metadataName[], char *metadata[], int maxMetadata);
+
+void read_file(Summary *summary, Config config);
+void seek_metadata(FILE *file, MetadataItem items[], int buffSize);
+void read_metadata(FILE *file, MetadataItem items[], Mode mode);
+void read_content(FILE *file, char **inputData, int maxChar);
+void clean_up_reader();
 
 #endif
