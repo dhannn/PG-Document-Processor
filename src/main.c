abcf115c4beb4f231360d20a5cb20a8b6e3addf4
Daniel Ramos III <daniel_ramos@dlsu.edu.ph>
2022-05-31 13:57:06 +0800

    nuked everything

--- src/main.c
+++ src/main.c
@@ -1,58 +1,6 @@
-#include "pgdocs.h"
-#include "deps/internals.h"
-#include <ctype.h>
-#include <string.h>
-#include <locale.h>
-
-const AnalyzerOption ANALYZER_OPTIONS[] = {
-    {"Word Count", get_word_count, report_token_frequency, "_wcount"},
-    {"N-Gram Count", get_ngram_count, report_token_frequency, "_ngram"},
-    {"Concordance", get_concordance, report_concordance, "_concordance"}
-};
+// #include <stdio.h>
 
 int main()
 {
-    Screen *screens = initialize_screens();   
-
-    display_screen(screens, 0);
-    // Summary summary;
-    // Config config;
-    // config.numChar = 0;
-
-    // printf("Choose a mode:\n");
-    // printf("  [1] Cleaning\n");
-    // printf("  [2] Analyis\n\n");
-
-    // int i; 
-    // scanf("%d", &i);
-
-    // set_mode(&summary, i - 1);
-
-    // printf("Choose an option:\n");
-    // for(int i = 0; i < MAX_ANALYZER_OPTIONS; i++) {
-    //     printf("  [%d] %s\n", i + 1, ANALYZER_OPTIONS[i].name);
-    // }
-    // scanf("%d", &i);
-
-    // set_options(&summary, config, i);
-
-    // summary.inFile = fopen("sample_files/cleaned_files/d.txt", "r");
-    // summary.outFile = fopen("meh.txt", "w");
-    // execute_summary(&summary, config);
-
-    // summary.inFile = fopen("sample_files/cleaned_files/d.txt", "r");
-    // summary.metadata.metadataItems[0].name = "Title";
-    // summary.metadata.metadataItems[1].name = "Author";
-    // summary.metadata.metadataItems[2].name = "Release Date";
-    // summary.metadata.metadataItems[3].name = "Language";
-    // summary.metadata.metadataItems[4].name = "Produced by";
-    // summary.options = 1;
-    // summary.outFile = fopen("samples_files/output/a_word_count.txt", "w");
-    // read_file(&summary, config);
-
-    // get_word_count(&summary, config);
-
-
-    destroy_screens(screens);
-    return 0;
+    
 }
--- src/pgdocs.h
+++ src/pgdocs.h
@@ -1,127 +1,36 @@
 #ifndef PG_DOCS_H
 #define PG_DOCS_H
 
-#include <stdio.h>
-#include <stdbool.h>
-
-typedef struct _tokenList TokenList;
-typedef struct _hashTable HashTable;
-
-#define NUM_OF_METADATA 5
-
-typedef struct {
-    const char *name;
-    char *data;
-} MetadataItem;
-
-typedef enum {
-    CLEAN, 
-    ANALYZE
-} Mode;
-
-#define MAX_METADATA_ITEMS 5
-
-typedef struct {
-    MetadataItem metadataItems[NUM_OF_METADATA];
-} Metadata;
-
-typedef struct {
-    int i;                      // placeholder for int variable
-    char *s;                    // placeholder for string variable
-} AdditionalOptions;
-
-typedef struct {
-    Mode mode;                  // processing mode (i.e. clean or analyze)
-    FILE *inFile;               // file pointer to input 
-    FILE *outFile;              // file pointer to output
-    unsigned int options;       // chosen option for cleaning or analysis
-    Metadata metadata;          // info about the document
-    char *inData;               // string of data to be processed
-    TokenList *tokenList;       // tokenized version of the input
-    char *outData;              // string of data to be reported
-    AdditionalOptions addOpts;  // additional options
-} Summary;
-
-typedef struct {
-    bool isInitialized;         // whether config is initialized or not
-    char *rawDocumentPath;      // path of raw PG documents
-    char *cleanedDocumentPath;  // path of cleaned documents
-    char *analysisOutputPath;   // path of output files (i.e., word count,...)
-    int numChar;                // number of character to be read from content
-    bool multiSelect;           // allow to choose more than one option
-} Config;
-
-typedef struct {
-    char *name;                                 // name of analysis option
-    void (*do_analysis)(Summary*, Config);      // function pointer to analysis
-    void (*report_analysis)(Summary*, Config);  // function pointer to report
-    char *fileSuffix;                           // name of analysis option
-} AnalyzerOption;
-
-typedef struct {
-    char *name;                                 // name of cleaning option
-    void (*clean)(Summary*, Config);            // function pointer to cleaning
-    void (*report_cleaned)(Summary*, Config);   // function pointer to report
-} CleanerOption;
+#define MAX_CHAR 1024
 
 typedef struct _screen Screen;
 
 typedef struct {
-    int index;
-    const char *optionName;
-    Screen *next_screen;
-    Screen *previous_screen;
+    char *optionName;
+    Screen *resultingScreen;
+    bool pointsToCommand;
 } ScreenOption;
 
-struct _screen{
-    const char **header;
-    int n;
-    char *input;
+struct _screen { 
+    char **header;
+    char *prompt;
     ScreenOption *options;
+    void *(get_input)();
+    int backIndex;
 };
 
-#define MAX_ANALYZER_OPTIONS 3
+typedef struct {
+    Screen *screen;
+    int nInput;
+    char strInput[MAX_CHAR];
+} ActiveScreen;
 
-TokenList *remove_duplicate_tokens(TokenList*);
-
-Metadata initialize_metadata();
-void set_metadata(MetadataItem items[], int metadataIndex, char *data);
-int get_metadata_index(MetadataItem metadataItems[], char *str);
-void delete_metadata(Metadata *metadata);
-
-Screen *initialize_screens();
-void display_screen(Screen *screen, int screenIndex);
-void destroy_screens(Screen* screen);
-
-void read_file(Summary *summary, Config config);
-void seek_metadata(FILE *file, MetadataItem items[], int buffSize);
-void read_metadata(FILE *file, MetadataItem items[], Mode mode);
-void read_content(FILE *file, char **inputData, int maxChar);
-void clean_up_reader();
-
-void set_mode(Summary *summary, Mode mode);
-void set_infile(Summary *summary, char *filename);
-void set_outfile(Summary *summary, char *filename);
-
-void set_options(Summary *summary, Config config, int rawInput);
-void set_additional_options(Summary *summary, AdditionalOptions addopts);
-void execute_summary(Summary *summary, Config config);
-
-void close_files(Summary *summary);
-void destroy_summary(Summary* summary);
-
-void analyze_data(Summary *summary, Config config);
-TokenList *convert_to_ngrams(TokenList *tl, int n);
-void get_word_count(Summary *summary, Config config);
-void report_token_frequency(Summary *summary, Config config);
-void get_ngram_count(Summary *summary, Config config);
-void report_ngram_count(Summary *summary, Config config);
-void get_concordance(Summary *summary, Config config);
-void report_concordance(Summary *summary, Config config);
-
-Metadata initialize_metadata();
-void delete_metadata(Metadata *metadata);
-
-extern const AnalyzerOption ANALYZER_OPTIONS[];
+void initialize_screens(Screen screens[]);
+void activate_screen(ActiveScreen* active, Screens screens[]);
+void __validate_screen_option(ActiveScreen *active);
+void go_to_screen(Screen *screens[], ActiveScreen *active);
+void get_int(ActiveScreen *active);
+void get_str(ActiveScreen *active);
+void print_metadata(char *metadataName, char *metadata);
 
 #endif
